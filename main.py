03527ff325b6ab76aef3b9ed3f459daf5d57acb7
Kamalesh <kamalcbe86@gmail.com>
2024-10-17 10:31:26 +0000

    Data stores permanently

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+import json
 
 app = FastAPI()
 
 feedbacks_data = {}
+
 
 class Feedback(BaseModel):
     name: str
@@ -16,45 +18,71 @@ class UpdateFeedback(BaseModel):
     data: Optional[str] = None
 
 
-@app.post("/feedback/{id}")
-def create_feedback(id: int, feedback: Feedback):
-    if id in feedbacks_data:
-        return {"error": "Feedback already exists"}
+def load_data():
+    try:
+        with open("feedbackData.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        with open("feedbackData.json", 'a') as f:
+            f.write("{}")
+        return json.load(f)
 
-    feedbacks_data[id] = feedback
+
+def save_data(data):
+    with open("feedbackData.json", "w") as f:
+        json.dump(data, f)
+
+
+@app.post("/feedback")
+def create_feedback(feedback: Feedback):
+    feedbacks_data = load_data()
+    if feedbacks_data:
+        id = max(map(int, feedbacks_data.keys())) + 1
+    else:
+        id = 1
+
+    feedbacks_data[id] = feedback.dict()
+    save_data(feedbacks_data)
     return feedbacks_data[id]
 
 
 @app.get("/feedbacks")
 def get_feedbacks():
+    feedbacks_data = load_data()
     return feedbacks_data
 
 
 @app.get("/feedback/{id}")
 def get_specific_feedback(id: int):
-    if id in feedbacks_data:
-        return feedbacks_data[id]
+    feedbacks_data = load_data()
+    if str(id) in feedbacks_data:
+        return feedbacks_data[str(id)]
+
     return {"error": "Feedback not available"}
 
 
 @app.put("/feedback/{id}")
 def update_feedback(id: int, feedback: UpdateFeedback):
-    if id not in feedbacks_data:
-        return {"error": "Feedback ID does not exist"}
+    feedbacks_data = load_data()
+    if str(id)  in feedbacks_data:
+        if feedback.name is not None:
+            feedbacks_data[str(id)]["name"] = feedback.name
 
-    if feedback.name is not None:
-        feedbacks_data[id].name = feedback.name
+        if feedback.data is not None:
+            feedbacks_data[str(id)]["data"] = feedback.data
 
-    if feedback.data is not None:
-        feedbacks_data[id].data = feedback.data
-
-    return feedbacks_data[id]
+        save_data(feedbacks_data)
+        return feedbacks_data[str(id)]
+        
+    return {"error": "Feedback ID does not exist"}
 
 
 @app.delete("/feedback/{id}")
 def delete_feedback(id: int):
-    if id not in feedbacks_data:
-        return {"error": "Feedback ID does not exist"}
-
-    del feedbacks_data[id]
-    return {"message": "Deleted successfully"}
+    feedbacks_data = load_data()
+    if str(id) in feedbacks_data:
+        del feedbacks_data[str(id)]
+        save_data(feedbacks_data)
+        return {"message": "Deleted successfully"}
+    
+    return {"error": "Feedback ID does not exist"}
